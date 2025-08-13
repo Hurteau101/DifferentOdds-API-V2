79f512b0ac25d1c2056269e25973ea4346a183af
Devon <devonhurteau@gmail.com>
2025-08-12 19:53:42 -0600

    WIP: Betr

---
+++ DFS/betr.py
@@ -0,0 +1,20 @@
+import aiohttp
+
+from Settings.book_base import SportbookRequestType
+from Settings.dfs_book_base import DFSBookBase
+
+
+class Betr(DFSBookBase):
+    def __init__(self):
+        super().__init__(SportbookRequestType.ASYNC, sportsbook_name="betr")
+
+
+    async def run_book(self):
+        async with aiohttp.ClientSession() as session:
+            api_data = await self.api_caller(
+                session=session,
+                url=self.book_data.url.get("main_url"),
+                method=self.book_data.method
+            )
+
+
--- Settings/dfs_providers.py
+++ Settings/dfs_providers.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Optional, Dict
+from typing import Optional, Dict, List
 
 
 # DFSProvider class to represent a DFS provider with its details.
@@ -9,6 +9,7 @@ class DFSProvider:
     url: dict
     method: str
     headers: Optional[Dict] = None
+    # payload_list: Optional[List[Dict]] = None
 
 # List of DFS providers with their configurations.
 DFS_PROVIDERS = [
@@ -25,5 +26,12 @@ DFS_PROVIDERS = [
             "main_url": "https://partner-api.prizepicks.com/projections"
         },
         method="GET"
+    ),
+    DFSProvider(
+        name="betr",
+        url={
+            "main_url": "https://betr-api.betr.app/graphql",
+        },
+        method="POST",
     )
 ]
