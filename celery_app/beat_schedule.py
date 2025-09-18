741ecd851914019a6ff012396ffb10256125b39d
Devon <devonhurteau@gmail.com>
2025-09-17 19:24:58 -0600

    Disabled Kalshi and updated beat and task

--- celery_app/beat_schedule.py
+++ celery_app/beat_schedule.py
@@ -14,11 +14,12 @@ for run_book_type, books in BOOKS.items():
         else:
             continue
 
+
         beat_schedule[f"run-{run_book_type}-{book_name}-every-{book_info['interval']}s"] = {
             "task": "celery_app.tasks.run_book",
             "schedule": book_info["interval"],
             "args": (book_name, redis_db, run_book_type),
-            "options": {"queue": queue, "expires": book_info["interval"] + 5},
+            "options": {"queue": queue},  # removed "expires"
         }
 
 
--- celery_app/tasks.py
+++ celery_app/tasks.py
@@ -16,7 +16,7 @@ from datetime import datetime, timezone
 from celery.utils.log import get_task_logger
 from celery import shared_task
 
-from Exchange.kalashi import Kalashi
+# from Exchange.kalashi import Kalashi
 from Redis.redis_manager import RedisManager
 from asgiref.sync import async_to_sync
 
