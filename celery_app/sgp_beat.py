32db08a4cdfdc2b69abdcf282357c136d296e9cc
Devon <devonhurteau@gmail.com>
2026-03-07 20:57:52 -0600

    WIP: Fixed test beat file

--- celery_app/sgp_beat.py
+++ celery_app/sgp_beat.py
@@ -3,7 +3,7 @@ from . import celery_app
 
 beat_schedule = {
     "run-autosgp": {
-        "task": "celery_app.autosgp_tasks.run_autosgp",
+        "task": "celery_app.sgp_tasks.run_autosgp",
         "schedule": timedelta(seconds=120),
         "options": {"expires": 3600},
     }
