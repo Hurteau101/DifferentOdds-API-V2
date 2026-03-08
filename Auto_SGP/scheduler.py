2206f5e9c1c275e40767c733b8a828e139db87a2
Devon <devonhurteau@gmail.com>
2026-03-07 21:19:28 -0600

    Changing to apscheduler for auto sgp

---
+++ Auto_SGP/scheduler.py
@@ -0,0 +1,22 @@
+import asyncio
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+
+from Auto_SGP.runner import AutoSGP
+
+
+async def run_autosgp():
+    autosgp = await AutoSGP.create()
+    await autosgp.runner()
+
+
+async def main():
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(run_autosgp, IntervalTrigger(seconds=120))
+    scheduler.start()
+
+    await asyncio.Event().wait()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
