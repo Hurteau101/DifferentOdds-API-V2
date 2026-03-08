c6103327e8d4e4fe5d044d0efaddc48301204e6a
Devon <devonhurteau@gmail.com>
2026-03-07 22:23:22 -0600

    WIP: Refactor - Enabling proxy for kambi

--- Books/SGP/kambi_sgp.py
+++ Books/SGP/kambi_sgp.py
@@ -18,23 +18,23 @@ class KambiSGP(SGPBookBase):
         bet_id_list = ",".join([bet_id.get("bet_id") for bet_id in self.link_data])
         event_id = next(iter({item["event_id"] for item in self.link_data}))
 
-            # Geo-Location issues, must use this proxy.
-            # proxy = os.getenv("KAMBI_PROXY")
-            # proxy_manger = ProxyManager(self.api_caller, proxies=[proxy])
+        # Geo-Location issues, must use this proxy.
+        proxy = os.getenv("KAMBI_PROXY")
+        proxy_manger = ProxyManager(self.api_caller, proxies=[proxy])
 
-            # api_data = await proxy_manger.proxy_caller(
-            #     book_name=self.book_data.name,
-            #     session=session,
-            #     url=self.book_data.url.get("main_url").format(event_id=event_id, bet_ids=bet_id_list),
-            #     method=self.book_data.method,
-            # )
-
-        api_data = await self.api_caller(
+        api_data = await proxy_manger.proxy_caller(
             book_name=self.book_data.name,
             session=session,
             url=self.book_data.url.get("main_url").format(event_id=event_id, bet_ids=bet_id_list),
             method=self.book_data.method,
         )
+
+        # api_data = await self.api_caller(
+        #     book_name=self.book_data.name,
+        #     session=session,
+        #     url=self.book_data.url.get("main_url").format(event_id=event_id, bet_ids=bet_id_list),
+        #     method=self.book_data.method,
+        # )
 
         if not api_data:
             return None
