e001b8a22be3df25d373366da1cb0948fc06cfc4
Devon <DevonHurteau@gmail.com>
2026-03-23 12:48:34 -0600

    Removed unecessary print statement

--- Auto_SGP/runner.py
+++ Auto_SGP/runner.py
@@ -168,7 +168,6 @@ class AutoSGP(APICaller):
 
             redis_key = f"{filter_selection.get('unique_name')}-{game_key}"
             if redis_key in previous_data:
-                print("Exists", redis_key)
                 continue
 
             if (
