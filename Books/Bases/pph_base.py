233e2909f4bfc234642ed5da640fa8970a0ed956
Devon <DevonHurteau@gmail.com>
2026-04-03 12:51:29 -0600

    Updated pph base class

--- Books/Bases/pph_base.py
+++ Books/Bases/pph_base.py
@@ -2,11 +2,12 @@ import json
 
 import requests
 from bs4 import BeautifulSoup
-
+from curl_cffi.requests import Session
 from Books.Bases.book_base import BookBase
 from Monitoring.monitoring import create_sentry_message
 from Utils.request_caller import SportbookRequestType
 from Books.Bases.sportsbook_base import SportsbooksBookBase
+
 
 class PPHBookBase(SportsbooksBookBase):
     def __init__(self, book_name: str, request_type: SportbookRequestType):
@@ -21,47 +22,41 @@ class PPHBookBase(SportsbooksBookBase):
         :param additional_headers: Any additional headers to include in the login request.
         :param login_key_word_check: A keyword to check in cookies to verify successful login.
         """
-
-        if not payload:
-            raise ValueError("Payload for login cannot be empty.")
-
-        login_url = self.book_data.url.get("login_url")
-        session = requests.Session()
-
-        scraper = cloudscraper.create_scraper(sess=session)
-        request_session = scraper.get(login_url)
-
-        if request_session.status_code != 200:
-            return {}
-
-        soup = BeautifulSoup(request_session.text, "html.parser")
-
         def find_values(name):
             hidden_tag = soup.find("input", {"name": name})
             return hidden_tag["value"] if hidden_tag else ""
 
-        starter_payload = {
-            "__VIEWSTATE": find_values("__VIEWSTATE"),
-            "__VIEWSTATEGENERATOR": find_values("__VIEWSTATEGENERATOR"),
-            "__EVENTVALIDATION": find_values("__EVENTVALIDATION"),
-        }
+        if not payload:
+            raise ValueError("Payload for login cannot be empty.")
+
+        with Session(impersonate="chrome120") as session:
+            response = session.get(self.book_data.url.get("login_url"))
+            soup = BeautifulSoup(response.text, "html.parser")
 
 
-        starter_payload.update(payload)
+            starter_payload = {
+                "__VIEWSTATE": find_values("__VIEWSTATE"),
+                "__VIEWSTATEGENERATOR": find_values("__VIEWSTATEGENERATOR"),
+                "__EVENTVALIDATION": find_values("__EVENTVALIDATION"),
+            }
 
-        if additional_headers:
-            self.book_data.headers.update(additional_headers)
+            starter_payload.update(payload)
 
-        session.post(login_url, data=payload, headers=self.book_data.headers)
+            if additional_headers:
+                self.book_data.headers.update(additional_headers)
 
-        if login_key_word_check and login_key_word_check not in session.cookies.get_dict():
-            create_sentry_message(
-                tag_key=self.book_data.name,
-                tag_value="login_failure",
-                message="Couldn't login",
-                level="error"
-            )
-            return None
+            print(starter_payload)
+            response = session.post("https://bettheguys.com/Login.aspx", data=payload, headers=self.book_data.headers)
 
-        print(session.cookies.get_dict())
-        return session.cookies.get_dict()
+            print(response.text)
+
+            if login_key_word_check and login_key_word_check not in session.cookies.get_dict():
+                create_sentry_message(
+                    tag_key=self.book_data.name,
+                    tag_value="login_failure",
+                    message="Couldn't login",
+                    level="error"
+                )
+                return None
+            print(session.cookies.get_dict())
+            return session.cookies.get_dict()
