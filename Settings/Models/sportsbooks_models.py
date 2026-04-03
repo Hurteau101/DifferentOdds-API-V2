1f4e1bc510d650d1bace7e60afcc0f35b0f2648b
Devon <DevonHurteau@gmail.com>
2026-04-03 12:55:21 -0600

    Added ordinal function, to ensure ordinal's like '1st' are displayed that way and not title case

--- Settings/Models/sportsbooks_models.py
+++ Settings/Models/sportsbooks_models.py
@@ -1,3 +1,4 @@
+import re
 from dataclasses import dataclass
 from typing import Optional
 
@@ -5,14 +6,29 @@ from Settings.Models.base_models import Stats, get_static_mapping
 from Utils.helpers import clean_structure
 
 
+def ordinal_formatter(market_name):
+    """Format the string if it is an ordinal market, otherwise return the original string."""
+    pattern = r'\b\d+(st|nd|rd|th)\b'
+    result = re.sub(pattern, lambda m: m.group(0), market_name, flags=re.IGNORECASE)
+
+    if re.search(pattern, market_name, flags=re.IGNORECASE):
+        result_split = result.split()
+
+        return f"{result_split[0]} {' '.join(result_split[1:]).title()}"
+
+    return market_name.title()
+
 # Use kw_only - As inheritance restriction.
 @dataclass(kw_only=True)
 class SportsbookStats(Stats):
     market: str
     bet_team: Optional[str] = None
     bet_player: Optional[str] = None
+    description: Optional[str] = None
+    modified_description: Optional[str] = None
+
 
     def __post_init__(self):
         self.bet_player = clean_structure(self.bet_player)
         self.bet_team = clean_structure(self.bet_team)
-        self.market = get_static_mapping().get("stats").get(self.market, self.market.title() if self.market else self.market)
+        self.market = get_static_mapping().get("stats").get(self.market, ordinal_formatter(self.market.lower()) if self.market else self.market)
