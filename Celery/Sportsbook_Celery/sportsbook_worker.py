c5f87904f7d00f345b004b4d3c52cb6ec67a3f8d
Devon <DevonHurteau@gmail.com>
2026-04-05 21:54:30 -0600

    Added buckeye2 to sportsbook worker

--- Celery/Sportsbook_Celery/sportsbook_worker.py
+++ Celery/Sportsbook_Celery/sportsbook_worker.py
@@ -23,6 +23,7 @@ from Books.Prediction_Liquidity.prophetx import Prophetx
 
 from Books.Sportsbooks.bet105 import Bet105
 from Books.Sportsbooks.ace import Ace
+from Books.Sportsbooks.buckeye_2 import Buckeye2
 from Books.Sportsbooks.metalic import Metallic
 from Books.Sportsbooks.onebv import OneBv
 from Books.Sportsbooks.sts import STS
@@ -263,6 +264,16 @@ BOOKS = {
     },
     "metallic": {
         "class": Metallic,
+        "interval": DEFAULT_INTERVAL,
+        "lock_timeout": DEFAULT_TIMEOUT,
+        "type": "sportsbook",
+        "task": "Sportsbook_Celery.sportsbook_worker.run_sportsbooks",
+        "soft_limit": SOFT_LIMIT,
+        "hard_limit": HARD_LIMIT,
+        "is_active": True,
+    },
+    "buckeye2": {
+        "class": Buckeye2,
         "interval": DEFAULT_INTERVAL,
         "lock_timeout": DEFAULT_TIMEOUT,
         "type": "sportsbook",
