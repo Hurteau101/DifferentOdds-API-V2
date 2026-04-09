11595872f6c2e850626e974c20832b6010d762bb
Devon <DevonHurteau@gmail.com>
2026-04-08 20:58:37 -0600

    Modified logic to check if any lines have moved more than 25 cents

--- Auto_SGP/checker.py
+++ Auto_SGP/checker.py
@@ -20,6 +20,27 @@ class Checker:
             redis_client=self.autosgp.endpoint_redis.redis_client
         )
 
+    def calculate_cent_movement(self, odds_1: float, odds_2: float, cent_movement_amount: int = 25):
+        def normalize_odds(odds: float):
+            """Convert everything to its distance from the 100 baseline"""
+            if abs(odds) == 100:
+                return 0
+
+            return abs(odds) - 100
+
+        # Check if we crossed the "Even" (100) line. One side is positive/underdog, the other is negative/favorite
+        different_sides = (odds_1 < 0 and odds_2 > 0) or (odds_1 > 0 and odds_2 < 0)
+
+        odds_1_normalized = normalize_odds(odds_1)
+        odds_2_normalized = normalize_odds(odds_2)
+
+        if different_sides:
+            # If we crossed from (-) to (+), we add the distances together
+            return (odds_1_normalized + odds_2_normalized) >= cent_movement_amount
+
+        # If we stayed on the same side, we find the difference
+        return abs(odds_1_normalized - odds_2_normalized) >= cent_movement_amount
+
 
     async def run_checker(self):
         redis_previously_stored_instance = RedisAsyncManager(database=10)
@@ -39,27 +60,33 @@ class Checker:
         previously_sent = set()
         endpoint = set()
 
-        for market in previous_data:
+        for market in previous_data[0:1]:
             for key, odds in market.get("raw_odds", {}).items():
                 previously_sent_key = market.get("key_mapper", {}).get(key)
                 endpoint_key = market.get("redis_key")
 
                 bettorodds_data = sportsbook_data.get(key)
+
+                # No data for this key, odds are likely removed. Remove data.
                 if not bettorodds_data:
-                    # print("No BettorOdds Data for Key: ", key)
                     previously_sent.add(previously_sent_key)
                     endpoint.add(endpoint_key)
                     continue
 
                 bettorodds_odds = self.autosgp.normalize_book_odds(bettorodds_data, valid_sgp_books)
                 compared_odds = DeepDiff(odds, bettorodds_odds)
-                if compared_odds.get("values_changed", {}):
-                    previously_sent.add(previously_sent_key)
-                    endpoint.add(endpoint_key)
-                    continue
 
-        # print(f"Previously Sent Keys to Remove: {previously_sent}")
-        # print(f"Endpoint Keys to Remove: {endpoint}")
+                if compared_odds.get("values_changed", {}):
+                    for key, change in compared_odds["values_changed"].items():
+                        old_value = change.get("old_value")
+                        new_value = change.get("new_value")
+
+                        if self.calculate_cent_movement(old_value, new_value):
+                            print(f"Cent Movement Detected for Key {key}: Old Value: {old_value}, New Value: {new_value}")
+                            previously_sent.add(previously_sent_key)
+                            endpoint.add(endpoint_key)
+                            break
+
         await self.remover(previously_sent, endpoint)
 
 
