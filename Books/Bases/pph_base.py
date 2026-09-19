05d2fd1600c5e69f68f25d7b9cb69ebe571d6755
Devon <DevonHurteau@gmail.com>
2026-09-19 15:12:05 -0600

    Fixed json path

--- Books/Bases/pph_base.py
+++ Books/Bases/pph_base.py
@@ -1,4 +1,3 @@
-import os
 from datetime import datetime
 from typing import Callable
 from rapidfuzz import process, fuzz, utils
@@ -14,7 +13,7 @@ class PPHBookBase(SportsbooksBookBase):
         self.mapping_configs = self._load_mapping_configs(book_name=book_name)
 
     def _load_mapping_configs(self, book_name: str):
-        with open(os.path.join(os.path.dirname(__file__), "..", "Sportsbooks", "mapping_configs.json"), "r") as f:
+        with open("Books/Sportsbooks/mapping_configs.json", "r") as f:
             mapping_configs = json.load(f)
 
         return mapping_configs.get(book_name)
